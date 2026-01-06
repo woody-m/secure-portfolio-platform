7a7fde685463bb35f9a632edee6e3ebe25be92dc
woody-m <bwoodymila@gmail.com>
2026-01-05 22:03:04 -0400

    Fix skills bubbles full-width animation on desktop | Corrige animação das bolhas para atravessar toda a tela no desktop

--- frontend/src/components/sections/Skills.jsx
+++ frontend/src/components/sections/Skills.jsx
@@ -1,20 +1,23 @@
 import './skills.css';
 
 const skills = [
-    { name: 'JavaScript', icon: '/src/assets/skills/javascript.svg', size: 140, top: '15%', duration: 34},
+    { name: 'JavaScript', icon: '/src/assets/skills/javascript.svg', size: 140, top: '15%', duration: 34 },
     { name: 'React', icon: '/src/assets/skills/react.svg', size: 100, top: '35%', duration: 30 },
     { name: 'Node', icon: '/src/assets/skills/node.svg', size: 80, top: '70%', duration: 22 },
     { name: 'Python', icon: '/src/assets/skills/python.svg', size: 95, top: '55%', duration: 26 },
     { name: 'GitHub', icon: '/src/assets/skills/github.svg', size: 60, top: '65%', duration: 18 },
-    { name: 'CSS', icon: '/src/assets/skills/css.svg', size: 65, top: '45%', duration: 24},
+    { name: 'CSS', icon: '/src/assets/skills/css.svg', size: 65, top: '45%', duration: 24 },
     { name: 'HTML', icon: '/src/assets/skills/html.svg', size: 100, top: '35%', duration: 21 },
-    { name: 'vite', icon: '/src/assets/skills/vite.svg', size: 65, top: '45%', duration: 20 },
+    { name: 'Vite', icon: '/src/assets/skills/vite.svg', size: 65, top: '45%', duration: 20 },
 ];
 
 function Skills() {
     return (
         <section className="skills-section" id="skills">
-            <h2>Skills</h2>
+            <h2 className="skills-title">
+                Technical <span>Skills</span>
+            </h2>
+
 
             <div className="skills-area">
                 {skills.map((skill, index) => (
